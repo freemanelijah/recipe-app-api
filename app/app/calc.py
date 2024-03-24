2118f5f664eda28565d758af93be28b9a7c1b9d7
hollow fit <hollowfit@pm.me>
2024-03-24 02:26:23 -0700

    configure db and TTD

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculator functions
+"""
+
+def add(x, y):
+    """Adds two numbers, returns the result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x from y."""
+    return y - x
